2f0e8b6b287a6690f97ab5d60676900040f03916
Mowgle <84435130+Mowgle88@users.noreply.github.com>
2022-08-01 18:52:24 +0300

    feat: add input

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,22 @@ import { ClassCounter } from './components/ClassCounter';
 import Counter from './components/Counter';
 
 function App() {
+  let [value, setValue] = useState('Some text here');
 
   return (
     <div className='App'>
       <ClassCounter />
       <Counter stringNumber='10' />
       <Counter stringNumber='20'/>
+
+      <div style={{marginTop: "20px"}}>
+        <input
+          type="text"
+          value={value}
+          onChange={event => setValue(event.target.value)}
+        />
+        <h1>{value}</h1>
+      </div>
     </div>
   );
 }
